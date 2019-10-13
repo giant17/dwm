afe69f1a54c90b34916974001b031dbe0dd6312a
Gianluca <giantadori@gmail.com>
2019-10-13 11:13:16 +0200

    Remove tags

--- config.h
+++ config.h
@@ -43,7 +43,8 @@ static const unsigned int alphas[][3]      = {
 };
 
 /* tagging  */
-static const char *tags[] = { "", "", "", ""};
+/* static const char *tags[] = { "", "", "", ""}; */
+static const char *tags[] = {""};
 
 static const Rule rules[] = {
 	/* xprop(1):
