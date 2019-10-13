85992933562e535261570602453a418dc6ba454a
Gianluca <giantadori@gmail.com>
2019-10-13 11:19:23 +0200

    No Bar as default

--- config.h
+++ config.h
@@ -5,7 +5,7 @@
 static const unsigned int borderpx  = 0;        /* border pixel of windows */
 static const unsigned int gappx     = 0;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
-static const int showbar            = 1;        /* 0 means no bar */
+static const int showbar            = 0;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 
 static const double defaultopacity  = 1;
