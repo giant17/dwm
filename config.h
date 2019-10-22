34fa0a495a2018efe36bb60dfbe8ef572acbb681
Gianluca <giantadori@gmail.com>
2019-10-22 14:36:33 +0200

    Newsboat and Neomutt shortcuts

--- config.h
+++ config.h
@@ -56,12 +56,20 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
+static const char *neomuttcmd[]  = { "st", "-e", "neomutt", NULL };
+static const char *newsboatcmd[]  = { "st", "-e", "newsboat", "-u", "$HOME/.local/share/newsboat/urls", NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_d,      spawn,          {.v = dmenucmd } },
 	{ MODKEY,                       XK_Return, spawn,          {.v = termcmd } },
-	{ MODKEY,                       XK_b,      togglebar,      {0} },
+
+	/* Apps */
+	{ MODKEY,                       XK_e,      spawn,          {.v = neomuttcmd } },
+	{ MODKEY,                       XK_n,      spawn,          {.v = newsboatcmd } },
+	{ MODKEY,                       XK_w,      focusstack,     {.i = +1 } },
+	{ MODKEY,                       XK_m,      focusstack,     {.i = +1 } },
+	{ MODKEY,                       XK_n,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
 	{ MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
